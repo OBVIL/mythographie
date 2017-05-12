ad15d6703fcee613044039aa80476d6748f05043
glorieux-f <frederic.glorieux@fictif.org>
2017-05-12 22:17:55 +0200

    Première salve de clés

--- data/auteurs.sh
+++ data/auteurs.sh
@@ -1,2 +1,3 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/../ && pwd )"
+echo $DIR
 xsltproc $DIR/tools/auteurs.xsl $DIR/*.xml > $DIR/test.csv
