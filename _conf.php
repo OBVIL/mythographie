ae6b04b184598b68dcb40fe625987b8cd3c28c25
glorieux-f <frederic.glorieux@fictif.org>
2017-09-19 19:55:11 +0200

    un peu de doc

--- _conf.php
+++ _conf.php
@@ -1,12 +1,11 @@
 <?php
 return array(
-  "srcdir" => dirname( __FILE__ ),
-  "destdir" => ".",
-  "cmdup" => "git pull",
-  "pass" => "VéroniqueGély",
-  // "srclist" => "obvil.csv",
-  "srcglob" => array( "xml/*_*.xml" ), // pour mise à jour de la polémique
-  "sqlite" => "mythographie.sqlite",
+  "title" => 'Mythographie', // Nom du corpus
+  "srcdir" => dirname( __FILE__ ), // dossier source depuis lequel exécuter la commande de mise à jour
+  "cmdup" => "git pull 2>", // commande de mise à jour
+  "pass" => "", // Mot de passe à renseigner obligatoirement à l’installation
+  "srcglob" => array( "xml/*_*.xml" ), // sources XML à publier
+  "sqlite" => "mythographie.sqlite", // nom de la base avec les métadonnées
   "formats" => "article, toc, epub, kindle",
 );
 ?>
